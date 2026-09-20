1e2da11dc26e9b9e122f493a43e1c181a98f593d
Vitor <vitortrevisan07@gmail.com>
2026-09-19 22:11:52 -0300

    Build indo!

--- src/app/components/Card/index.tsx
+++ src/app/components/Card/index.tsx
@@ -1,9 +1,7 @@
 import { FaRegStar, FaStarHalfAlt, FaStar } from "react-icons/fa";
 import { Dispatch, SetStateAction } from "react";
-<<<<<<< HEAD
-=======
 import Image from "next/image";
->>>>>>> d37a426 (To exausto, não vou escrever o que fiz não, até porque sei que vai da erro e vou ter que fazer mais uns 20 commit)
+
 import type { Film } from "../../types";
 
 type Props = {
@@ -15,43 +13,33 @@ const Card = ({ film, setDetailsId }: Props) => {
   const rating = Number(film.vote_average) / 2;
 
   const fullStars = Math.floor(rating);
-
   const hasHalfStar = rating % 1 >= 0.5;
-
   const emptyStars = 5 - fullStars - (hasHalfStar ? 1 : 0);
 
   return (
     <main
-      className="w-full h-auto flex flex-col justify-between transition-transform duration-200 hover:scale-105 cursor-pointer bg-[#1B1B1B] rounded-xl"
+      className="flex h-auto w-full cursor-pointer flex-col justify-between rounded-xl bg-[#1B1B1B] transition-transform duration-200 hover:scale-105"
       onClick={() => {
-        console.log("CLICOU:", film.id);
         setDetailsId(film.id);
       }}
     >
       <figure className="w-full">
-<<<<<<< HEAD
-        <img
-          src={`https://image.tmdb.org/t/p/w500${film.poster_path}`}
-          alt={film.title}
-          className="w-full h-[10em] sm:h-[20em] sm:pb-4 rounded-t-xl"
-=======
         <Image
           src={`https://image.tmdb.org/t/p/w342${film.poster_path}`}
           alt={film.title}
           width={200}
           height={300}
-          className="object-cover w-full h-[10em] sm:h-[20em] sm:pb-4 rounded-t-xl"
->>>>>>> d37a426 (To exausto, não vou escrever o que fiz não, até porque sei que vai da erro e vou ter que fazer mais uns 20 commit)
+          className="h-[10em] w-full rounded-t-xl object-cover sm:h-[20em] sm:pb-4"
         />
       </figure>
 
       <h1 className="pb-2 pl-2">{film.title}</h1>
 
-      <p className="text-[#f5f5f590] pl-2 text-sm sm:text-[1rem]">
+      <p className="pl-2 text-sm text-[#f5f5f590] sm:text-[1rem]">
         {film.overview.slice(0, 100) + "..."}
       </p>
 
-      <div className="w-fit flex items-end gap-1 mt-2 text-yellow-400 bg-[#1B1B1B] rounded-xl p-2 pl-2">
+      <div className="mt-2 flex w-fit items-end gap-1 rounded-xl bg-[#1B1B1B] p-2 pl-2 text-yellow-400">
         {Array.from({ length: fullStars }).map((_, i) => (
           <FaStar key={`full-${i}`} />
         ))}
@@ -62,7 +50,7 @@ const Card = ({ film, setDetailsId }: Props) => {
           <FaRegStar key={`empty-${i}`} />
         ))}
 
-        <span className="text-xs text-white/70 ml-1">
+        <span className="ml-1 text-xs text-white/70">
           ({Number(film.vote_average).toFixed(1)})
         </span>
       </div>
